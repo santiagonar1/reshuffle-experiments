9023d713dad6170f8cd43c263afbf9db00bcaa52
Santiago Narvaez <santiagonar1@gmail.com>
2026-04-25 11:57:00 +0200

    feat: include MPI library

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+#include <mpi.h>
+
 int main() {
+
+    MPI_Init(nullptr, nullptr);
+
     std::cout << "Hello World!" << std::endl;
+
+    MPI_Finalize();
 
     return 0;
 }
