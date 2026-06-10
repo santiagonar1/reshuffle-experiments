8c0b9707f38954e686afe18180d6e0b4ce28b044
Santiago Narvaez <santiagonar1@gmail.com>
2026-06-10 14:57:15 -0700

    feat(benchmark): add static assertions to validate num processors

--- benchmarks/include/benchmark_config.hpp
+++ benchmarks/include/benchmark_config.hpp
@@ -28,4 +28,17 @@ namespace change_block {
     constexpr auto FINAL_BLOCK_SIZE = 10;
 }// namespace change_block
 
+
+static_assert(common::EXPECTED_NUM_PROCESSORS ==
+              gather::INITIAL_NUM_PROCESSORS_PER_DIMENSION *
+                      gather::INITIAL_NUM_PROCESSORS_PER_DIMENSION);
+
+static_assert(common::EXPECTED_NUM_PROCESSORS ==
+              scatter::FINAL_NUM_PROCESSORS_PER_DIMENSION *
+                      scatter::FINAL_NUM_PROCESSORS_PER_DIMENSION);
+
+static_assert(common::EXPECTED_NUM_PROCESSORS ==
+              change_block::INITIAL_NUM_PROCESSORS_PER_DIMENSION *
+                      change_block::FINAL_NUM_PROCESSORS_PER_DIMENSION);
+
 #endif//RESHUFFLE_EXPERIMENTS_COMMON_HPP
