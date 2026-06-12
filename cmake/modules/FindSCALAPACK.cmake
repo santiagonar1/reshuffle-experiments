4d860db1f3908fd616753bc0d5ef0cbbad2cdef2
Santiago Narvaez <santiagonar1@gmail.com>
2026-06-12 11:23:41 -0700

    feat(cmake): add logic for detecting and linking BLACS libraries
    Some distros do not bundle ScaLAPACK and BLACS together. Thus, to avoid
    any surprise, I separated the logic to find each one.

--- cmake/modules/FindSCALAPACK.cmake
+++ cmake/modules/FindSCALAPACK.cmake
@@ -18,6 +18,19 @@ if (NOT SCALAPACK_FOUND)
     )
 endif ()
 
+# Some distros separate BLACS from ScaLAPACK, so we make this logic explicit if needed
+find_library(BLACS_LIBRARIES
+        NAMES blacs-openmpi blacs-mpi blacs
+        HINTS
+        ENV BLACS_ROOT
+        ENV BLACS_DIR
+        PATH_SUFFIXES lib lib64
+)
+
+if (BLACS_LIBRARIES)
+    list(APPEND SCALAPACK_LIBRARIES ${BLACS_LIBRARIES})
+endif ()
+
 find_package_handle_standard_args(SCALAPACK
         REQUIRED_VARS SCALAPACK_LIBRARIES
         FOUND_VAR SCALAPACK_FOUND
@@ -33,6 +46,12 @@ if (SCALAPACK_FOUND AND NOT TARGET SCALAPACK::SCALAPACK)
     if (SCALAPACK_INCLUDE_DIRS)
         set_target_properties(SCALAPACK::SCALAPACK PROPERTIES
                 INTERFACE_INCLUDE_DIRECTORIES "${SCALAPACK_INCLUDE_DIRS}"
+        )
+    endif ()
+
+    if (BLACS_LIBRARIES)
+        set_target_properties(SCALAPACK::SCALAPACK PROPERTIES
+                INTERFACE_LINK_LIBRARIES "${BLACS_LIBRARIES}"
         )
     endif ()
 endif ()
